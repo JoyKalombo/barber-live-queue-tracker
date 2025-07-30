7ee07de09c44705cf81b3d8b01830f4510bb9547
JoyKalombo <112102600+JoyKalombo@users.noreply.github.com>
2025-07-30 19:02:14 +0100

    trying to push for separate sites for each barber and keeping the get_barber_id function from my sessions.py file

--- utils/session.py
+++ utils/session.py
@@ -5,6 +5,14 @@ import streamlit as st
 from utils.firebase_utils import get_barber_config
 
 
+def get_barber_id(default="default_barber"):
+    barber = st.query_params.get("barber")
+    if not barber:
+        st.warning("⚠️ No barber selected. Returning to default.")
+        return default
+    return barber
+
+
 def get_barber_context(default="default_barber"):
     barber_id = st.query_params.get("barber", default)
     config = get_barber_config(barber_id)
